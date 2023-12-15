16f1a8dfa36205a8d05d00242f0cccb859704718
nguyentanphat <ntphat_21th@student.agu.edu.vn>
2023-12-16 00:24:27 +0700

    add sua_taikhoan.php

---
+++ shop-quan-ao/views/trang_con/sua_taikhoan.php
@@ -0,0 +1,24 @@
+<div class="card mx-auto d-block" style="width: 450px;">
+	<div class="card-header text-center pb-1">
+		<h5><i class="fas fa-user"></i> TÀI KHOẢN CỦA TÔI</h5>
+	</div>
+	<div class="card-body">
+		<form method="POST" action="index.php?q=xu-ly-sua-tai-khoan">
+			<input type="text" name="id" value="<?php echo $_GET['id'] ?>" hidden>
+			<div class="form-group">
+				<label>TÊN ĐĂNG NHẬP</label>
+				<input type="text" class="form-control" value="<?php echo $data['taikhoan']['tendangnhap'] ?>" readonly>
+			</div>
+			<div class="form-group">
+				<label for="exampleFormControlSelect1">LOẠI TÀI KHOẢN</label>
+				<select class="form-control" id="exampleFormControlSelect1" name="loaitk" required>
+					<option value="member" <?php if($data['taikhoan']['loaitk'] == 'member') echo 'selected'; ?>>member</option>
+					<option value="admin" <?php if($data['taikhoan']['loaitk'] == 'admin') echo 'selected'; ?>>admin</option>
+				</select>
+			</div>
+			<div class="text-center">
+				<button type="submit" class="btn btn-primary">Cập nhật</button>
+			</div>
+		</form>
+	</div>
+</div>
