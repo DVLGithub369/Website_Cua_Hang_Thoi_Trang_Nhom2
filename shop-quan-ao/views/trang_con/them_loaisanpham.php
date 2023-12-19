0f2eb12e3a6ca0c8f25433ae87808cc1ce2c5896
dth206092 <ntp.clam.ft@gmail.com>
2023-12-19 10:49:52 +0700

    add them_sanpham.php

---
+++ shop-quan-ao/views/trang_con/them_loaisanpham.php
@@ -0,0 +1,13 @@
+<div class="card mx-auto d-block" style="width: 450px;">
+	<div class="card-body">
+		<form method="POST" action="index.php?q=xu-ly-them-loai-san-pham">
+			<div class="form-group">
+				<label>TÊN LOẠI</label>
+				<input type="text" class="form-control" name="ten" required>
+			</div>
+			<div class="text-center">
+				<button type="submit" class="btn btn-primary">Lưu dữ liệu</button>
+			</div>
+		</form>
+	</div>
+</div>
