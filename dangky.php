548d5484375d131ebb519ffa85e12eb37fe48bc7
LE DUONG <153206012+DVLGithub369@users.noreply.github.com>
2023-12-19 22:46:03 +0700

    Cap nhat

---
+++ dangky.php
@@ -0,0 +1,20 @@
+<div class="card mx-auto d-block" style="width: 450px;">
+	<div class="card-header text-center pb-1">
+		<h5><i class="fas fa-user-plus"></i> ĐĂNG KÝ TÀI KHOẢN</h5>
+	</div>
+	<div class="card-body">
+		<form method="POST" action="index.php?q=xu-ly-dang-ky">
+			<div class="form-group">
+				<label>TÊN ĐĂNG NHẬP</label>
+				<input type="text" class="form-control" name="tendangnhap" required>
+			</div>
+			<div class="form-group">
+				<label>MẬT KHẨU</label>
+				<input type="password" class="form-control" name="matkhau" required>
+			</div>
+			<div class="text-center">
+				<button type="submit" class="btn btn-primary">Đăng ký</button>
+			</div>
+		</form>
+	</div>
+</div>
